79888d520eac913fa79ba7677d5a08e8fa724e8b
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-05-08 09:08:04 -0400

    attempting to solve cors issue

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-//const jobNum = document.getElementById('job-number');
+const jobNum = document.getElementById('job-number');
 document.getElementById('subButton').addEventListener('click', getTime);
 
 // url will be: http://vmmkweb/jobinfo/jobinfo.aspx?jobno=${jobNum}
+// 221163 -- Sample job number
 
 function getTime(){
   // bring in our dependancies, require these packages
   let axios = require('axios');
   let cheerio = require('cheerio');
-  let fs = require('fs');
 
   // now scrape by making a get request with axios
-  axios.get('http://www.espncricinfo.com')
+  // Axios can be used in both browser and node.js platforms
+
+  // this first get request will be used to scrape and make the url we need
+  /*
+  Example URL to scrape for:
+  http://vmmkweb/jobinfo/dumpgenbomdwfcgi-4.aspx?BOMDATE=5-8-2019&PROJECT=221163&QUERY=100&FORMAT=1&ITEM=L709C00000000000&TOP=L709C00000000000
+  */
+  axios.get("http://vmmkweb/jobinfo/jobinfo.aspx?jobno=221163")
     .then((response) => {
         if(response.status === 200) {
+          // sucsessfull call to get
           const html = response.data;
           const $ = cheerio.load(html);
 
-          const headline = $('.headlineStack');
-          const title = headline.find('h1').text();
-          console.log(title);
+          const jobTable = $('#tbl_JobInfo');
+          const table = jobTable.find('table');
+          console.log(table.text());
 
     }
     }, (error) => console.log(err) );
